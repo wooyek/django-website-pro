2628c6d388ae728e1807f3b8d7fc8df4ce98ed0d
Janusz Skonieczny <git@wooyek.com>
2016-05-10 11:27:12 +0200

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = parse_requirements(
 with open("README.rst") as readme:
     long_description = readme.read()
 
-version = "0.2.2"
+version = "0.2.3"
 
 setup_kwargs = {
     'name': "{{ project_name|slugify }}",
